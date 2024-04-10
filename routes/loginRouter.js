175474f27faf690e11ae2e228a25319d3ec6adf0
Jaya-Sandeep-Ketha <kethajayasandeep1254@gmail.com>
2024-04-10 13:33:38 -0400

    Captcha Fix

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const loginRouter = express.Router(); 
 //const {userVerification} = require('../middleware/auth')
-const { login, logout, register, googleAuth, facebookAuth, captcha} = require("../controllers/authcontroller"); // Adjust the path as necessary
+const { login, logout, register, googleAuth, facebookAuth} = require("../controllers/authcontroller"); // Adjust the path as necessary
 loginRouter.post("/register", register);
 loginRouter.post("/login", login);
 loginRouter.post("/logout", logout);
