1e77a9d4721eba48974ec7b07649bc025bc11db2
Jaya-Sandeep-Ketha <kethajayasandeep1254@gmail.com>
2024-04-10 13:18:36 -0400

    Image Captcha testing

--- routes/loginRouter.js
+++ routes/loginRouter.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const loginRouter = express.Router(); 
 //const {userVerification} = require('../middleware/auth')
-const { login, logout, register, googleAuth, facebookAuth} = require("../controllers/authcontroller"); // Adjust the path as necessary
+const { login, logout, register, googleAuth, facebookAuth, captcha} = require("../controllers/authcontroller"); // Adjust the path as necessary
 loginRouter.post("/register", register);
 loginRouter.post("/login", login);
+loginRouter.post("/check-captcha", captcha);
 loginRouter.post("/logout", logout);
 loginRouter.post("/auth/google", googleAuth);
 loginRouter.post("/auth/facebook", facebookAuth);
